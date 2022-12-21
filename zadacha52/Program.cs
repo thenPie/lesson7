77226ae46ed5b431fc3657719f2862466885c749
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-21 21:42:02 +0300

    cleaned zadacha52

--- zadacha52/Program.cs
+++ zadacha52/Program.cs
@@ -22,38 +22,11 @@ for(int i = 0; i < rows; i++) {
 }
 
 Console.WriteLine();
-// int[] aver = new int[] {twoDarray[0, 0], twoDarray[1, 0], twoDarray[2, 0]};
-// Console.WriteLine($"{aver[0]} {aver[1]} {aver[2]}");
-// double avg = Queryable.Average(aver.AsQueryable());
-// Console.WriteLine($"{avg}");
-
 double smm = 0;
 for(int o = 0; o < columns; o++) {
     for(int i = 0; i < rows; i++) {
         smm = smm + twoDarray[i, o];
-        Console.Write($"{twoDarray[i, o]}, ");
     }
-    Console.WriteLine(smm);
-    Console.WriteLine(smm / rows);
+    Console.WriteLine($"{smm / rows} —> среднее колонки №{o + 1}");
     smm = 0;
-}
-// Console.WriteLine(smm);
-// Console.WriteLine(smm / rows);
-
-// for(int i = 0; i < rows; i++) {
-//     for(int o = 0; o < columns; o++) {
-//        int[] aver = new int[] {twoDarray[i, o], twoDarray[i, o + 1], twoDarray[i, o + 2]};
-//        double avg = Queryable.Average(aver.AsQueryable());
-//        Console.WriteLine("Average = {0} ", avg);
-//     }
-//     // int[] aver = new int[] {twoDarray[0, i], twoDarray[0, i + 1], twoDarray[0, i + 2]};
-//     // double avg = Queryable.Average(aver.AsQueryable());
-//     // Console.Write("Average = {0} ", avg);
-// }
-
-Console.WriteLine();
-for(int i = 0; i < columns; i++) {
-    for(int o = 0; o < rows; o++) {
-        Console.Write($"{twoDarray[o, i]} ");
-    }
 }
