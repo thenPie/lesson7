e577c16e53d28ff07c496e655ac6589a83b51ede
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-21 13:25:40 +0300

    Initial commit, did zadacha47

---
+++ zadacha47/Program.cs
@@ -0,0 +1,25 @@
+﻿int Random() {
+    Random rnd = new Random();
+    int num = rnd.Next(10, 100);
+    return num;
+}
+
+Console.Write("Сначала введите количество строк и затем количество столбиков через пробел: ");
+var input = Console.ReadLine();
+var space = input.Split(" ");
+int rows = int.Parse(space[0]);
+int columns = int.Parse(space[1]);
+
+int[,] twoDarray = new int[rows, columns];
+
+for(int i = 0; i < rows; i++) {
+    Console.Write("[");
+    for(int o = 0; o < columns; o++) {
+        twoDarray[i, o] = Random();
+        if(o == columns - 1) {
+            Console.WriteLine($"{twoDarray[i, o]}]");
+        } else {
+            Console.Write($"{twoDarray[i, o]}, ");
+        }
+    }
+}
